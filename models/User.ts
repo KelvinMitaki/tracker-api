5256d6291cf8b118217e3a00956916a323dbfcdd
KelvinMitaki <mitakikelvin1@gmail.com>
2021-01-29 11:58:20 +0300

    added a user schema

---
+++ models/User.ts
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+interface UserAttrs {
+  email: string;
+  password: string;
+}
+
+interface UserDoc extends mongoose.Document {
+  email: string;
+  password: string;
+}
+
+interface UserModel extends mongoose.Model<UserDoc> {
+  build(attrs: UserAttrs): UserDoc;
+}
+
+const UserSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      unique: true,
+      required: true
+    },
+    password: {
+      type: String,
+      required: true
+    }
+  },
+  { timestamps: true }
+);
+
+UserSchema.statics.build = (attrs: UserAttrs) => {
+  return new User(attrs);
+};
+
+const User = mongoose.model<UserDoc, UserModel>("User", UserSchema);
+
+export { User };
