72788d128f45136677a5e2f9fddedce4994c7fb5
KelvinMitaki <mitakikelvin1@gmail.com>
2021-01-29 12:20:10 +0300

    wired up signup endpoint

--- routes/authRoutes.ts
+++ routes/authRoutes.ts
@@ -1,9 +1,39 @@
 import { Router } from "express";
+import { check } from "express-validator";
+import { validate } from "../middlewares/validation";
+import bcrypt from "bcrypt";
+import { User } from "../models/User";
 
 const router = Router();
 
-router.post("/signup", async (req, res) => {
-  res.send("post req");
-});
+router.post(
+  "/signup",
+  check("email").trim().isEmail().withMessage("enter a valid email"),
+  check("password")
+    .trim()
+    .isLength({ min: 6 })
+    .withMessage("password must be six characters min"),
+  validate,
+  async (req, res) => {
+    try {
+      const { email, password } = req.body as { [key: string]: string };
+      const userExists = await User.exists({ email });
+      if (userExists) {
+        return res
+          .status(403)
+          .send({ message: "user with that email already exists" });
+      }
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const user = User.build({
+        email: email.toLowerCase(),
+        password: hashedPassword
+      });
+      await user.save();
+      res.send(user);
+    } catch (error) {
+      res.status(501).send({ message: error });
+    }
+  }
+);
 
 export { router as authRoutes };
