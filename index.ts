bc20b48645d3e859d8f19ee83a652877940dc830
KelvinMitaki <mitakikelvin1@gmail.com>
2021-01-29 11:22:07 +0300

    connected to db

--- index.ts
+++ index.ts
@@ -1,5 +1,20 @@
 import express from "express";
+import mongoose from "mongoose";
 
 const app = express();
 
+const connectMongo = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI!, {
+      useNewUrlParser: true,
+      useFindAndModify: false,
+      useCreateIndex: true,
+      useUnifiedTopology: true
+    });
+    console.log("connected to the db");
+  } catch (error) {
+    console.log(error);
+  }
+};
+connectMongo();
 app.listen(3000, () => console.log(`server started on port 3000`));
